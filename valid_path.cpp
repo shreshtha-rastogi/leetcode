9b90d5c164d3a12d282783c33fdcbce849a2811b
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-04-27 21:44:08 +0530

    first commit

---
+++ valid_path.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<vector<vector<int>>> dir = {
+            {},
+            {{0,-1},{0,1}},
+            {{-1,0},{1,0}},
+            {{0,-1},{1,0}},
+            {{0,1},{1,0}},
+            {{0,-1},{-1,0}},
+            {{0,1},{-1,0}}
+        };
+        
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        q.push({0,0});
+        vis[0][0] = 1;
+        
+        while(!q.empty()) {
+            auto [x,y] = q.front();
+            q.pop();
+            
+            if(x == m-1 && y == n-1) return true;
+            
+            for(auto &d : dir[grid[x][y]]) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+                
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n || vis[nx][ny]) continue;
+                
+                for(auto &back : dir[grid[nx][ny]]) {
+                    if(nx + back[0] == x && ny + back[1] == y) {
+                        vis[nx][ny] = 1;
+                        q.push({nx, ny});
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+};
