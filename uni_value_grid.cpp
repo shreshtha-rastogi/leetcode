5396edad8a218d758ef898efd4b06712ec401f34
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-04-28 15:29:13 +0530

    first commit

---
+++ uni_value_grid.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> v;
+        
+        for (auto &row : grid) {
+            for (auto &val : row) {
+                v.push_back(val);
+            }
+        }
+        
+        int rem = v[0] % x;
+        for (int num : v) {
+            if (num % x != rem) return -1;
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int n = v.size();
+        int median = v[n / 2];
+        
+        int operations = 0;
+        for (int num : v) {
+            operations += abs(num - median) / x;
+        }
+        
+        return operations;
+    }
+};
