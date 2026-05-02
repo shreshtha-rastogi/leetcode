d97a86aee7c3799c68c3eaa98d6341bdb35c4af8
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-02 16:07:52 +0530

    first commit

--- rotated_digits.cpp
+++ rotated_digits.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 using namespace std;
-
 class Solution {
 public:
     bool isGood(int x) {
         bool diff = false;
-
         while (x > 0) {
             int d = x % 10;
-
             // invalid digits
             if (d == 3 || d == 4 || d == 7) return false;
-
             // digits that change after rotation
             if (d == 2 || d == 5 || d == 6 || d == 9)
                 diff = true;
-
             x /= 10;
         }
-
         return diff;
     }
-
     int rotatedDigits(int n) {
         int count = 0;
         for (int i = 1; i <= n; i++) {
