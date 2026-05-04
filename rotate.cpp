492248861f0ad6550fe0b84f908d37b9dc4af75d
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-04 14:49:53 +0530

    first commit

---
+++ rotate.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<vector<int>> matrix(n, vector<int>(n));
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> matrix[i][j];
+        }
+    }
+    
+    Solution obj;
+    obj.rotate(matrix);
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
