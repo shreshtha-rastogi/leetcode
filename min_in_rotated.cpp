79d99d2f8b1638aad56c24da840145f0fb9216f9
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-16 14:33:41 +0530

    adding changes

---
+++ min_in_rotated.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findSmallest(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+        long long res = 1;
+        for (int x : arr) {
+            if (x > res)
+                break;
+            res += x;
+        }
+        return res;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    Solution obj;
+
+    cout << obj.findSmallest(arr);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
