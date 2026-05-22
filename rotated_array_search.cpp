4245ff41931663b0ac09141c029a67fea3ddd519
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-22 12:24:42 +0530

    adding changes

---
+++ rotated_array_search.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half sorted
+            if (nums[low] <= nums[mid]) {
+
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Right half sorted
+            else {
+
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target element: ";
+    cin >> target;
+
+    Solution obj;
+
+    int ans = obj.search(nums, target);
+
+    cout << "Index of target: " << ans << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
