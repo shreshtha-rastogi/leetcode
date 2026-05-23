36461b6ebb61068df7c0ce8a53bf0f95e8fdd592
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-23 12:04:13 +0530

    adding changes

---
+++ check_array.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+
+    if (obj.check(nums)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
