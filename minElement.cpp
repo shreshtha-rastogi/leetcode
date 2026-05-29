0e63f5e463c0b4f076d6a3807b6668ed017972de
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-05-29 12:56:11 +0530

    adding changes

---
+++ minElement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int ans = INT_MAX;
+
+        for (int num : nums) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            ans = min(ans, sum);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    int result = obj.minElement(nums);
+
+    cout << "Minimum element after digit sum replacement: " << result << endl;
+
+    return 0;
+}
