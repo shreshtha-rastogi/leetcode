e2b0708bcf94fa4c6c788c83273fb1f68cb95de9
shreshtha-rastogi <786shreshtharastogi@gmail.com>
2026-07-16 22:21:29 +0530

    change comitted

---
+++ practice_linked_list.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+    Node(int val, Node* nxt) {
+        data = val;
+        next = nxt;
+    }
+};
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    Node* y = Node(arr[0], nullptr);
+    cout<<y->data<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
